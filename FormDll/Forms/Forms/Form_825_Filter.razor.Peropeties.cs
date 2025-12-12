0679ba27635f0a230cef00caacb49bf9fb3f9fce
saeid ghadiri <saeidghadiri@outlook.com>
2025-12-12 13:42:12 +0330

    14040921_3

--- FormDll/Forms/Forms/Form_825_Filter.razor.Peropeties.cs
+++ FormDll/Forms/Forms/Form_825_Filter.razor.Peropeties.cs
@@ -80,7 +80,7 @@ namespace Forms.Forms
         /// <summary>
         ///  ورژنی که فرم باهاش ساخته شده
         /// </summary>
-        public string? VersionForm { get; set; } = "5209";
+        public string? VersionForm { get; set; } = "5211";
 
 
         /// <summary>
